f444935b36dbd2d39194a35755800c67232a763b
Flight3R <michaal747@gmail.com>
2022-09-07 16:57:55 +0200

    a little bit of refactor

--- classes.h
+++ classes.h
@@ -2,78 +2,102 @@
 #include <vector>
 #include <string>
 
-
-class Car {
-private:
-    string name;
-    int mass;
-    float velocity;
-    float surfaceArea;
-
-    Position *position;
-    Engine *engine;
-    GearBox *gearBox;
-    TyreSet *tyreSet;
-    Atmosphere *atmosphere;
-
-public:
-    Car(string, int, float, float, Position*, Engine*, GearBox*, TyreSet*, Atmosphere*);
-
-    float calculateCurrentVelocity(float);
-    void setVelocity(float);
-};
-
-class Position {
-private:
-    float range;
-
-public:
-    Position(float);
-
-    float calculatePosition(float, float);
-    void setRange(float);
-};
-
+/************************************
+ *          Class Engine
+ ************************************/
 class Engine {
 private:
     int RPM;
     int minRPM;
     int maxRPM;
-    vector<int> torqueMap;
+    std::vector<float> torqueMap;
 
     float interpolateTorque();
 
-public:    
-    Engine(int, int, int, vector<int>);
+public:
+    Engine(const int&, const int&, const int&, const std::vector<float>&);
+    Engine(const Engine&&);
+    Engine(const Engine&) = delete;
 
     float calculateCurrentPower();
     float setRPM();
 };
 
-class GearBox {
+/************************************
+ *          Class Position
+ ************************************/
+class Position {
 private:
-    vector<int> ratioMap;
+    float range;
 
 public:
-    GearBox(vector<int>);
+    Position(const float&);
 
-    float calculateRPM(float, float);
+    float calculatePosition(const float&, const float) const;
+    void setRange(const float&);
 };
 
+/************************************
+ *          Class GearBox
+ ************************************/
+class GearBox {
+private:
+    std::vector<int> ratioMap;
+
+public:
+    GearBox(const std::vector<int>&);
+    GearBox(const GearBox&&);
+    GearBox(const GearBox&) = delete;
+
+    float calculateRPM(const float&, const float&) const;
+};
+
+/************************************
+ *          Class TyreSet
+ ************************************/
 class TyreSet {
 private:
     float diameter;
 
 public:
-    TyreSet(float);
+    TyreSet(const float&);
 };
 
+/************************************
+ *          Class Atmosphere
+ ************************************/
 class Atmosphere {
 private:
     float airDensity;
 
 public:
-    Atmosphere(float);
+    Atmosphere(const float&);
 
-    float calculateDrag(float, float);
+    float calculateDrag(const float&, const float&) const;
+};
+
+/************************************
+ *          Class Car
+ ************************************/
+class Car {
+private:
+    std::string name;
+    int mass;
+    float velocity;
+    float surfaceArea;
+
+    Position position;
+    Engine engine;
+    GearBox gearBox;
+    TyreSet tyreSet;
+    const Atmosphere* atmospherePtr;
+
+public:
+    Car(const char*, const int&, const float&, const float&, const Position&, const Engine&,
+        const GearBox&, const TyreSet&, const Atmosphere* const);
+    Car(const Car&&);
+    Car(const Car&) = delete;
+
+    float calculateCurrentVelocity(const float&);
+    void setVelocity(const float&);
 };
--- methods.cpp
+++ methods.cpp
@@ -1,34 +1,121 @@
 #include "classes.h"
 #include <cmath>
-using namespace std; 
 
-<template T>
-T interpolate(T x0, T y0, T x1, T y1, T x) {
+template <typename T>
+T interpolate(T x0, T y0, T x1, T y1, T x)
+{
     T linearInterpolation = (y1 - y0) / (x1 - x0) * (x - x0) + y0;
     return linearInterpolation;
 }
 
-float Engine::interpolateTorque() {
-    int lowPoint = RPM % 100 * 100;
-    int highPoint = lowPoint + 100;
-    return interpolate<float> (lowPoint, torqueMap[(startRPM + lowPoint) / 10], highPoint, torqueMap[(startRPM + highPoint) / 10]);
+/************************************
+ *          Class Engine
+ ************************************/
+Engine::Engine(const int& RPM, const int& minRPM, const int& maxRPM, const std::vector<float>& torqueMap)
+    :RPM(RPM), minRPM(minRPM), maxRPM(maxRPM), torqueMap(std::move(torqueMap))
+{
 }
 
-float Engine::calculateCurrentPower() {
+Engine::Engine(const Engine&& other)
+    :RPM(other.RPM), minRPM(other.minRPM), maxRPM(other.maxRPM), torqueMap(std::move(other.torqueMap))
+{
+}
+
+float Engine::interpolateTorque()
+{
+    float lowPoint = RPM % 100 * 100;
+    float highPoint = lowPoint + 100;
+    return interpolate<float>(lowPoint,
+                              torqueMap[(RPM + lowPoint) / 10],
+                              highPoint,
+                              torqueMap[(RPM + highPoint) / 10],
+                              (float)RPM);
+}
+
+float Engine::calculateCurrentPower()
+{
     return interpolateTorque() * RPM;
 }
 
-float Position::calculatePosition(float speed, float timeDelta=0.01) {
+/************************************
+ *          Class Position
+ ************************************/
+Position::Position(const float& range)
+    :range(range)
+{
+}
+
+float Position::calculatePosition(const float& speed, const float timeDelta=0.01) const
+{
     return range + speed * timeDelta;
 }
 
-void Position::setRange(float range) {
+void Position::setRange(const float& range)
+{
     this->range = range;
 }
 
-float Car::calculateCurrentVelocity(float timeDelta) {
-    float enginePower = engine->calculateCurrentPower();
-    float dragPower = velocity * atmosphere->calculateDrag(velocity, surfaceArea);
+/************************************
+ *          Class GearBox
+ ************************************/
+GearBox::GearBox(const std::vector<int>& ratioMap)
+    :ratioMap(std::move(ratioMap))
+{
+}
+
+GearBox::GearBox(const GearBox&& other)
+    :ratioMap(std::move(other.ratioMap))
+{
+}
+
+float GearBox::calculateRPM(const float& velocity, const float& diameter) const
+{
+    return velocity / diameter / M_PI;
+}
+
+/************************************
+ *          Class TyreSet
+ ************************************/
+TyreSet::TyreSet(const float& diameter)
+    : diameter(diameter)
+{
+}
+
+/************************************
+ *          Class Atmosphere
+ ************************************/
+Atmosphere::Atmosphere(const float& airDensity)
+{
+    this->airDensity = airDensity;
+}
+
+float Atmosphere::calculateDrag(const float& velocity, const float& surfaceArea) const
+{
+    return (airDensity * pow(velocity, 3)) / 2 * surfaceArea;
+}
+
+/************************************
+ *          Class Car
+ ************************************/
+Car::Car(const char* name, const int& mass, const float& velocity, const float& surfaceArea, const Position& position,
+         const Engine& engine, const GearBox& gearBox, const TyreSet& tyreSet, const Atmosphere* const atmospherePtr)
+    :name(name), mass(mass), velocity(velocity), surfaceArea(surfaceArea), position(std::move(position)),
+        engine(std::move(engine)), gearBox(std::move(gearBox)), tyreSet(std::move(tyreSet)),
+        atmospherePtr(atmospherePtr)
+{
+}
+
+Car::Car(const Car&& other)
+    :name(std::move(other.name)), mass(other.mass), velocity(other.velocity), surfaceArea(other.surfaceArea), position(std::move(other.position)),
+        engine(std::move(other.engine)), gearBox(std::move(other.gearBox)), tyreSet(std::move(other.tyreSet)),
+        atmospherePtr(other.atmospherePtr)
+{
+}
+
+float Car::calculateCurrentVelocity(const float& timeDelta)
+{
+    float enginePower = engine.calculateCurrentPower();
+    float dragPower = velocity * atmospherePtr->calculateDrag(velocity, surfaceArea);
     float effectivePower = enginePower - dragPower;
     if (effectivePower < 0) {
         return velocity;
@@ -38,49 +125,7 @@ float Car::calculateCurrentVelocity(float timeDelta) {
     }
 }
 
-void Car::setVelocity(float velocity) {
+void Car::setVelocity(const float& velocity)
+{
     this->velocity = velocity;
-}
-
-float Atmosphere::calculateDrag(float velocity, float surfaceArea) {
-    return (airDensity * pow(velocity, 3)) / 2 * surfaceArea;
-}
-
-float GearBox::calculateRPM(float velocity, float diameter) {
-    return velocity / diameter / M_PI;
-}
-
-Car::Car(string name, int mass, float velocity, float surfaceArea, Position *position, Engine *engine, GearBox *gearBox, TyreSet *tyreSet, Atmosphere *atmosphere) {
-    this->name = name;
-    this->mass = mass;
-    this->velocity = velocity;
-    this->surfaceArea = surfaceArea;
-    this->position = position;
-    this->engine = engine;
-    this->gearBox = gearBox;
-    this->tyreSet = tyreSet;
-    this->atmosphere = atmosphere;
-}
-
-Atmosphere::Atmosphere(float airDensity) {
-    this->airDensity = airDensity;
-}
-
-Position::Position(float range) {
-    this->range = range;
-}
-
-Engine::Engine(int RPM, int minRPM, int maxRPM, vector<int> torqueMap) {
-    this->RPM = RPM;
-    this->minRPM = minRPM;
-    this->maxRPM = maxRPM;
-    this-> torqueMap = torqueMap;
-}
-
-TyreSet::TyreSet(float diameter) {
-    this->diameter = diameter;
-}
-
-GearBox::GearBox(vector<int> ratioMap) {
-    this->ratioMap = ratioMap;
 }
